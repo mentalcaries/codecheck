c06d6fea8acf539dec0c2031815e3e5b21b52f86
mentalcaries <neexis@gmail.com>
2025-09-25 08:18:01 -0400

    read download directory

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/mentalcaries/tt-codecheck/internal/config"
+)
+
+type state struct {
+  config *config.Config
+}
 
 func main() {
-  fmt.Println("Hello, world")
+  cfg, err := config.Read()
+
+  if err != nil {
+    log.Fatalf("error reading config: %v", err)
+  }
+
+  appState := &state{
+    config: &cfg,
+  }
+
+  fmt.Println(appState.config.DownloadDirectory)
 }
