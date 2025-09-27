4b8094abcbb95ebb57512e95ada70d9f7f7e1a69
mentalcaries <neexis@gmail.com>
2025-09-27 19:07:25 -0400

    overwrite or create new repo directory

--- cmd/app/handler_review.go
+++ cmd/app/handler_review.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 func isValidGitHubURL(link string) bool {
@@ -30,15 +33,42 @@ func dirExists(path string) bool {
 
 func setupTempDir(path string) (string, error) {
 	if !dirExists(path) {
-		fmt.Println("directory does not exist, creating directory...")
+		fmt.Println("Directory does not exist, creating directory...")
 		err := os.MkdirAll(path, 0755)
 		if err != nil {
 			return "", fmt.Errorf("Could not create user directory")
 		}
 	}
-	fmt.Println("directory exists, using it...")
 
 	return path, nil
+}
+
+func cloneRepository(link, path string) error {
+	cmd := exec.Command("git", "clone", link, path)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			return fmt.Errorf("git clone failed (exit %d)", ee.ExitCode())
+		}
+		return fmt.Errorf("failed to run git: %w", err)
+	}
+	return nil
+}
+
+func deleteExistingRepo(path string) error {
+	cmd := exec.Command("rm", "-rf", path)
+	if err := cmd.Run(); err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			return fmt.Errorf("failed to delete local repo (exit %d)", ee.ExitCode())
+		}
+		return fmt.Errorf("failed to delete directory: %w", err)
+	}
+	fmt.Println("Existing directory deleted...")
+	return nil
+
 }
 
 func handlerReview(s *state, cmd command) error {
@@ -54,7 +84,7 @@ func handlerReview(s *state, cmd command) error {
 		return fmt.Errorf("Invalid github URL")
 	}
 
-	userName, _ := extractRepoDetails(repositoryLink)
+	userName, repoName := extractRepoDetails(repositoryLink)
 
 	userDirPath := filepath.Join(s.config.DownloadDirectory, userName)
 
@@ -62,6 +92,30 @@ func handlerReview(s *state, cmd command) error {
 	if err != nil {
 		return fmt.Errorf("Could not create temp directory: %v", err)
 	}
+
+	localRepoPath := filepath.Join(userDirPath, repoName)
+	for dirExists(localRepoPath) {
+		fmt.Print("Repo already exists locally. Overwrite? y/n/q: ")
+		input := bufio.NewScanner(os.Stdin)
+		input.Scan()
+		if strings.ToLower(input.Text()) == "y" {
+			deleteExistingRepo(localRepoPath)
+		}
+
+		if strings.ToLower(input.Text()) == "n" {
+			localRepoPath = localRepoPath + "-copy"
+			for dirExists(localRepoPath) {
+				localRepoPath = localRepoPath + "-copy"
+			}
+		}
+
+		if strings.ToLower(input.Text()) == "q" {
+			return fmt.Errorf("User cancelled operation")
+		}
+
+	}
+
+	err = cloneRepository(repositoryLink, localRepoPath)
 
 	return nil
 }
