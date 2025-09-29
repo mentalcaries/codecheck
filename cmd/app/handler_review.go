5b4dae58f6958244e4f5687dcf69b3f7c1129822
mentalcaries <neexis@gmail.com>
2025-09-28 22:36:08 -0400

    run dev server or file server and open VSCode

--- cmd/app/handler_review.go
+++ cmd/app/handler_review.go
@@ -3,11 +3,14 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path/filepath"
 	"regexp"
 	"strings"
+	"syscall"
 )
 
 func isValidGitHubURL(link string) bool {
@@ -25,10 +28,7 @@ func extractRepoDetails(link string) (string, string) {
 
 func dirExists(path string) bool {
 	_, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-	return true
+	return err == nil
 }
 
 func setupTempDir(path string) (string, error) {
@@ -71,29 +71,7 @@ func deleteExistingRepo(path string) error {
 
 }
 
-func handlerReview(s *state, cmd command) error {
-
-	if len(cmd.args) < 1 {
-		return fmt.Errorf("link to github repository is required")
-	}
-
-	repositoryLink := cmd.args[0]
-
-	isValidLink := isValidGitHubURL(repositoryLink)
-	if !isValidLink {
-		return fmt.Errorf("Invalid github URL")
-	}
-
-	userName, repoName := extractRepoDetails(repositoryLink)
-
-	userDirPath := filepath.Join(s.config.DownloadDirectory, userName)
-
-	userDirPath, err := setupTempDir(userDirPath)
-	if err != nil {
-		return fmt.Errorf("Could not create temp directory: %v", err)
-	}
-
-	localRepoPath := filepath.Join(userDirPath, repoName)
+func setupLocalRepo(repositoryLink, localRepoPath string) (string, error) {
 	for dirExists(localRepoPath) {
 		fmt.Print("Repo already exists locally. Overwrite? y/n/q: ")
 		input := bufio.NewScanner(os.Stdin)
@@ -110,12 +88,145 @@ func handlerReview(s *state, cmd command) error {
 		}
 
 		if strings.ToLower(input.Text()) == "q" {
-			return fmt.Errorf("User cancelled operation")
+			return "", fmt.Errorf("User cancelled operation")
 		}
-
 	}
 
-	err = cloneRepository(repositoryLink, localRepoPath)
+	err := cloneRepository(repositoryLink, localRepoPath)
+	if err != nil {
+		return "", fmt.Errorf("Could not clone repo: %v", err)
+	}
+	return localRepoPath, nil
+}
 
+func isVSCodeInstalled() bool {
+	_, err := exec.LookPath("code")
+	return err == nil
+}
+
+func openRepoWithVSCode(repoPath string) error {
+	if isVSCodeInstalled() {
+		fmt.Println("VS Code available. Opening project...")
+		cmd := exec.Command("code", repoPath)
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("Error opening VSCode")
+		}
+	} else {
+		fmt.Println("VS Code CLI not found")
+	}
+
+	return nil
+}
+
+func hasPackageJSON(dirPath string) bool {
+	packagePath := filepath.Join(dirPath, "package.json")
+	_, err := os.Stat(packagePath)
+	return err == nil
+}
+
+func installDependencies(dirPath string) error {
+	cmd := exec.Command("npm", "install")
+	cmd.Dir = dirPath
+	cmd.Stderr = os.Stderr
+
+	fmt.Println("Installing NPM depedencies...")
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("Could not install dependencies: %w", err)
+	} else {
+		fmt.Println("Depedencies installed successfully.")
+	}
+
+	return nil
+}
+
+func startDevServer(dirPath string) error {
+	cmd := exec.Command("npm", "run", "dev")
+	cmd.Dir = dirPath
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	fmt.Println("\n>>>Starting dev server...")
+	if err := cmd.Start(); err != nil {
+		if ee, ok := err.(*exec.ExitError); ok {
+			return fmt.Errorf("Failed to start the dev server (exit %d)", ee.ExitCode())
+		}
+		return fmt.Errorf("Could not start dev server: %w", err)
+	}
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+		<-c
+		fmt.Println("\n>>>Dev server stopped.")
+
+		if cmd.Process != nil {
+			cmd.Process.Kill()
+		}
+	}()
+	return nil
+}
+
+func startFileServer(dirPath string) error {
+	fileserver := http.FileServer(http.Dir(dirPath))
+	http.Handle("/", fileserver)
+
+	fmt.Println("Serving index.html on Port 5500. Access the project here: http://localhost:5500")
+
+	go func() {
+		if err := http.ListenAndServe(":5500", nil); err != nil {
+			fmt.Errorf("Could not start file server: %v", err)
+		}
+	}()
+	return nil
+}
+
+func handlerReview(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return fmt.Errorf("link to github repository is required")
+	}
+
+	repositoryLink := cmd.args[0]
+
+	isValidLink := isValidGitHubURL(repositoryLink)
+	if !isValidLink {
+		return fmt.Errorf("Invalid github URL")
+	}
+
+	userName, repoName := extractRepoDetails(repositoryLink)
+	userDirPath := filepath.Join(s.config.DownloadDirectory, userName)
+
+	userDirPath, err := setupTempDir(userDirPath)
+	if err != nil {
+		return fmt.Errorf("Could not create temp directory: %v", err)
+	}
+
+	localRepoPath := filepath.Join(userDirPath, repoName)
+
+	localRepoPath, err = setupLocalRepo(repositoryLink, localRepoPath)
+	if err != nil {
+		return fmt.Errorf("Could not set up local directory: %v", err)
+	}
+
+	if hasPackageJSON(localRepoPath) {
+		fmt.Println("\nReading package.json...")
+		if err = installDependencies(localRepoPath); err != nil {
+			return fmt.Errorf("%v", err)
+		}
+		if err = startDevServer(localRepoPath); err != nil {
+			return fmt.Errorf("%v", err)
+		}
+	} else {
+		if err = startFileServer(localRepoPath); err != nil {
+			return fmt.Errorf("%v", err)
+		}
+	}
+
+	openRepoWithVSCode(localRepoPath)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	fmt.Println("Shutting down.. Goodbye!")
 	return nil
 }
